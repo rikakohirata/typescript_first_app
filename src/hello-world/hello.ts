44fca8e908274d4a98522065663b5a952a481057
rikakohirata <rikako.hirata88@gmail.com>
2023-05-18 13:02:41 +0900

    判別可能なユニオン型

--- src/hello-world/hello.ts
+++ src/hello-world/hello.ts
@@ -169,3 +169,27 @@ if (typeof maybeUserId === "string") {
   console.log(userId);  // => ユーザー１
 }
 
+// 複雑なユニオン型の絞り込み（ネガティブな例）
+// type UploadStatus = InProgress | Success | Failure;
+// type InProgress = { done: boolean; progress: number };
+// type Success = { done: boolean };
+// type Failure = { done: boolean; error: Error };
+
+function printStatus(status: UploadStatus) {
+  if (status.done) {
+    if ("error" in status) {
+      console.log(`アップロード失敗:${status.error.message}`);
+    } else {
+      console.log("アップロード成功");
+    }
+  } else if ("progress" in status) {
+    console.log(`アップロード中:${status.progress}%`);
+  }
+}
+
+// 判別可能なユニオン型
+// ディスクリミネータtypeを追加
+type UploadStatus = InProgress | Success | Failure;
+type InProgress = { type: "InProgress"; progress: number };
+type Success = { type: "Success" };
+type Failure = { type: "Failure"; error: Error };
